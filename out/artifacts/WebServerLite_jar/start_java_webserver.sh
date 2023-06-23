3e2b9a9708b3fc2751ecb8e59d76753d1f10d22b
I.Myasnikov <miacom@mail.ru>
2023-06-23 20:06:54 +0700

    add jar

--- out/artifacts/WebServerLite_jar/start_java_webserver.sh
+++ out/artifacts/WebServerLite_jar/start_java_webserver.sh
@@ -1,2 +1,2 @@
 # Запуск JavaServer
-java -jar /data/data/com.termux/files/home/WebServerLite_jar/WebServerLite.jar /data/data/com.termux/files/home/WebServerLite_jar/config.ini
+$PREFIX/share/bin/java -jar /data/data/com.termux/files/home/WebServerLite_jar/WebServerLite.jar /data/data/com.termux/files/home/WebServerLite_jar/config.ini
