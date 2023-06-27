8858171179c411d23de6b980b513977c6af2a186
I.Myasnikov <miacom@mail.ru>
2023-06-27 14:54:03 +0700

    run SH edit

--- out/artifacts/WebServerLite_jar/start_java_webserver.sh
+++ out/artifacts/WebServerLite_jar/start_java_webserver.sh
@@ -1,2 +1,2 @@
 # Запуск JavaServer
-$PREFIX/share/bin/java -jar /data/data/com.termux/files/home/WebServerLite_jar/WebServerLite.jar /data/data/com.termux/files/home/WebServerLite_jar/config.ini
+java -jar /data/data/com.termux/files/home/WebServerLite_jar/WebServerLite.jar /data/data/com.termux/files/home/WebServerLite_jar/config.ini
